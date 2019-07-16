c57bb60ed188590b55c0ed411da9d120331d27c0
Tona González <martiuh@gmail.com>
2019-07-16 11:17:36 -0500

    test: more testing features and configurations

---
+++ server/__tests__/serverCheck.js
@@ -0,0 +1,83 @@
+/* eslint-disable global-require */
+const path = require("path");
+
+describe("server check", () => {
+  let loopback, boot;
+  const mockApp = {};
+  const getApp = () => require("../server");
+
+  beforeEach(() => {
+    jest.resetModules();
+    jest.mock("loopback");
+    jest.mock("loopback-boot");
+    loopback = require("loopback");
+    boot = require("loopback-boot");
+    loopback.mockReturnValue(mockApp);
+  });
+
+  it("instances loopback", () => {
+    getApp();
+    expect(loopback).toBeCalled();
+  });
+
+  it("adds start function", () => {
+    expect(getApp().start).toBeInstanceOf(Function);
+  });
+
+  it("boot application", () => {
+    getApp();
+    expect(boot).toBeCalledWith(
+      mockApp,
+      expect.objectContaining({
+        appRootDir: path.join(__dirname, "..")
+      }),
+      expect.any(Function)
+    );
+  });
+
+  it("throws when application boot fails", () => {
+    boot.mockImplementation((a, o, done) => done("foo"));
+    expect(getApp).toThrowError("foo");
+  });
+
+  it("not throws when application boot success", () => {
+    boot.mockImplementation((a, o, done) => done());
+    expect(getApp).not.toThrow();
+  });
+
+  it("calls express listen when start is called", () => {
+    const listen = jest.fn();
+    loopback.mockReturnValue({ listen });
+    getApp().start();
+    expect(listen).toBeCalledWith(expect.any(Function));
+  });
+
+  it("emit started when start is called", () => {
+    const get = jest.fn(() => "foo");
+    const listen = jest.fn(cb => cb());
+    const emit = jest.fn();
+    loopback.mockReturnValue({ listen, get, emit });
+    getApp().start();
+    expect(emit).toBeCalledWith("started");
+  });
+
+  it("log api endpoint when start is called", () => {
+    const get = jest.fn(() => "foo/api");
+    const listen = jest.fn(cb => cb());
+    const emit = jest.fn();
+    console.log = jest.fn();
+    loopback.mockReturnValue({ listen, get, emit });
+    getApp().start();
+    expect(console.log).toBeCalledWith(expect.any(String), "foo/api");
+  });
+
+  it("log explorer url when start is called", () => {
+    const get = jest.fn(term => (term === "url" ? "url" : { mountPath: "foo" }));
+    const listen = jest.fn(cb => cb());
+    const emit = jest.fn();
+    console.log = jest.fn();
+    loopback.mockReturnValue({ listen, get, emit });
+    getApp().start();
+    expect(console.log).toBeCalledWith(expect.any(String), "url", "foo");
+  });
+});
