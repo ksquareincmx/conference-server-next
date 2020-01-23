b63d7ac2dd3ac29f025eab6d7e90963164d9eba8
Eduardo Monforte <eduardo.monforte@ksquareinc.com>
2020-01-23 15:46:51 -0600

    Improved .env example and added a subsection on the .env file to the README.

--- server/devBootScripts/initialData.js
+++ server/devBootScripts/initialData.js
@@ -1,43 +1,96 @@
-const moment = require('moment');
+// const moment = require('moment');
 
-module.exports = App => {
-    const { Room, user, Booking } = App.models;
-    Room.create(
-      {
-        name: "Kijimi",
-        bg_color: "gray",
-        txt_color: "blue"
-      }, (err, kijimiRoom) => {
-        if (err) {
-          throw new Error(`Cannot create Room`, err);
-        }
+// module.exports = App => {
+//     const { Room, user, Booking } = App.models;
+//     Room.create(
+//       {
+//         name: "Kijimi",
+//         bg_color: "gray",
+//         txt_color: "blue"
+//       }, (err, kijimiRoom) => {
+//         if (err) {
+//           throw new Error(`Cannot create Room`, err);
+//         }
 
-        user.create(
-            {
-                name: "Babu Frik",
-                role: "admin",
-            }, (err, babu) => {
-                if (err) {
-                    throw new Error('Cannot create user', err);
-                }
+//         user.create(
+//             {
+//                 name: "Babu Frik",
+//                 role: "admin",
+//             }, (err, babu) => {
+//                 if (err) {
+//                     throw new Error('Cannot create user', err);
+//                 }
 
-            Booking.create(               
-                {
-                    description: "He-hey!",
-                    start: moment().set( { hour: 10, minute: 15 } ).valueOf(),
-                    end: moment().set( { hour: 10, minute: 45 } ).valueOf(),
-                    room_id: kijimiRoom.id,
-                    user_id: babu.id
-                }, (err, newBooking) => {
-                  if (err) {
-                    throw new Error('Cannot create booking', err);
-                  }
-                }
-            )
+//             Booking.create(               
+//                 {
+//                     description: "He-hey!",
+//                     start: moment().set( { hour: 10, minute: 15 } ).valueOf(),
+//                     end: moment().set( { hour: 10, minute: 45 } ).valueOf(),
+//                     room_id: kijimiRoom.id,
+//                     user_id: babu.id
+//                 }, (err, newBooking) => {
+//                   if (err) {
+//                     throw new Error('Cannot create booking', err);
+//                   }
+//                 }
+//             )
 
-            }
-        )
-      }
-    );
-  };
+//             }
+//         )
+//       }
+//     );
+//   };
 
+const moment = require("moment-timezone");
+
+module.exports = async App => {
+  const { user, Booking, Room } = App.models;
+
+  const FirstUser = await user.create({
+    name: "Test User",
+    email: "conferenceshenanigans@gmail.com",
+    password: "password",
+    role: "admin"
+  });
+
+  const rooomsArr = await Room.create([
+    {
+      name: "DeWitt",
+      bg_color: "#333",
+      txt_color: "#e3e3e3"
+    },
+    {
+      name: "Skywalker",
+      bg_color: "#333",
+      txt_color: "#e3e3e3"
+    },
+    {
+      name: "Ganondorf",
+      bg_color: "#333",
+      txt_color: "#e3e3e3"
+    },
+    {
+      name: "Stark",
+      bg_color: "#333",
+      txt_color: "#e3e3e3"
+    },
+    {
+      name: "Dumbledore",
+      bg_color: "#333",
+      txt_color: "#e3e3e3"
+    },
+    {
+      name: "Wayne",
+      bg_color: "#333",
+      txt_color: "#e3e3e3"
+    }
+  ]);
+
+  await Booking.create({
+    description: "1st booking",
+    end: moment().add("30", "minutes"),
+    start: moment().add("10", "minutes"),
+    user_id: FirstUser.id,
+    room_id: rooomsArr[0].id
+  });
+};
