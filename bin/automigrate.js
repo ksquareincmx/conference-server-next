d959afceac1d1170696915a240d23149106cbd1c
Eduardo Monforte <eduardo.monforte@ksquareinc.com>
2020-02-13 18:04:39 -0600

    Automigration and persistent mock data completed.

---
+++ bin/automigrate.js
@@ -0,0 +1,55 @@
+const path = require("path");
+
+const app = require(path.resolve(__dirname, "../server/server"));
+const allModels = require("../server/model-config.json");
+const {
+  user: initialUsers,
+  Room: initialRooms
+} = require("./initialData.json");
+
+const datasource = app.datasources.conferenceBooking;
+
+const excludeModels = ["_meta", "User"];
+
+const automigrate = async startAutomigration => {
+  await executeAutomigration();
+  await populateInitialData();
+  await disconnectDatasource();
+  console.log("Complete!");
+};
+
+// step 1:
+
+async function executeAutomigration() {
+  const models = Object.keys(allModels).filter(
+    model => !excludeModels.includes(model)
+  );
+
+  const migratePromises = models.map(model => datasource.automigrate(model));
+
+  return Promise.all(migratePromises);
+}
+
+// step 2:
+
+function populateInitialData() {
+  function createMockData(model, dataSet) {
+    return Promise.all(
+      dataSet.map(async element => {
+        await model.create(element);
+        console.log("Created data:", element);
+      })
+    );
+  }
+
+  return Promise.all([
+    createMockData(app.models.Room, initialRooms),
+    createMockData(app.models.user, initialUsers)
+  ]);
+}
+
+// step 3:
+
+const disconnectDatasource = () => datasource.disconnect();
+
+automigrate();
--- server/boot/adminUser.js
+++ server/boot/adminUser.js
@@ -5,21 +5,21 @@ const { app } = require("../config/config");
 const { make } = require('../services/jwtService');
 const { generateRandomValueBase64 } = require('../utils')
 
-module.exports = App => {
-  const { user } = App.models;
-  user.create(
-    {
-      name: app.admin.name,
-      email: app.admin.email,
-      password: generateRandomValueBase64(20),
-      role: "admin"
-    }, async (err, user) => {
-      if (err) {
-        throw new Error(`Cannot create admin`, err);
-      }
+// module.exports = App => {
+//   const { user } = App.models;
+//   user.create(
+//     {
+//       name: app.admin.name,
+//       email: app.admin.email,
+//       password: generateRandomValueBase64(20),
+//       role: "admin"
+//     }, async (err, user) => {
+//       if (err) {
+//         throw new Error(`Cannot create admin`, err);
+//       }
 
-      console.log('Admin token: ', `"${await make(user)}"`);
+//       console.log('Admin token: ', `"${await make(user)}"`);
             
-    }
-  );
-};
+//     }
+//   );
+// };
